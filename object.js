cfaa84454d519ca4eab838ced8ddf75a68e7deb9
imrancoder10 <imranhossain1148@gmail.com>
2024-09-04 18:47:37 +0600

    object

---
+++ object.js
@@ -0,0 +1,28 @@
+//primitive
+//basic, primary
+const age = 20;
+const school = 'JC school';
+const isPassed = true;
+// let isDeveloper;
+// const subject = ['Bangla', 'English', 'physics', 'Math'];
+// object
+//non primitive
+const bottle = {
+    brand: 'apple',
+    price: 45,
+    color: 'white',
+    isClean: false
+}
+
+//non primitive
+
+const subject = {
+    name: 'biology',
+    teacher: 'Imran hossain',
+    examDate: '30 Feb',
+    chapter: ['first', 'second', 'third'],
+    exams: {
+        name: 'Final Exam',
+        marks: 100
+    }
+}
