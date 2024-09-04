8ea5edbcf8dd2aef255c3dfd6ffadd9777d86ce9
imrancoder10 <imranhossain1148@gmail.com>
2024-09-04 17:56:41 +0600

    concat and includes

---
+++ concat.js
@@ -0,0 +1,9 @@
+const first = 'Imran';
+const last = 'Hossain';
+
+// const fullName = first + ' ' + last;
+const fullName = first.concat(' ').concat(last);
+console.log(fullName);
+
+// includes
+console.log(last.includes('j'));
