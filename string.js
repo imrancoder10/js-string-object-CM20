21426512bed25588c55c37fbb32381fec3662056
imrancoder10 <imranhossain1148@gmail.com>
2024-09-04 12:45:56 +0600

    different between string and object

---
+++ string.js
@@ -0,0 +1,31 @@
+const country = 'Bangladesh'; //string
+const division = "NewKhale"; //string
+const district = `B-Baria`; //string
+// const thana = new String('Demta'); //string
+
+// console.log(typeof country);
+// console.log(typeof division);
+// console.log(typeof district);
+// console.log(typeof thana);
+// console.log(thana);
+// ================Array declaration===========================
+// Array is mutable --> you can change the array elements.
+
+const numbers = [58, 86, 48, 46, 96];
+// console.log(numbers.length);
+// console.log(numbers[4]);
+// console.log(numbers);
+numbers[1] = 500;
+// console.log(numbers);
+
+// =========================string============================
+// string is immutable --> not changeable
+
+const capital = 'Dhaka';
+// console.log(capital);
+// console.log(capital.length);
+// console.log(capital[1]);
+console.log(capital);
+capital[1] = 'Q'; // string is immutable --> not changeable.
+console.log(capital);
+
