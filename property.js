95b3162c15aa4e53e419b61c35dc266c4ce3f9d8
imrancoder10 <imranhossain1148@gmail.com>
2024-09-04 09:19:46 +0600

    property

---
+++ property.js
@@ -0,0 +1,25 @@
+const person = {
+    name: 'Imran hossain',
+    age: 25,
+    profession: 'developer',
+    salary: 25000,
+    married: true,
+    'fav places': ['bandorban', 'saintmartin', 'kuakata']
+}
+// console.log(person);
+//dot notation
+// console.log(person.profession);
+const income = person.salary;
+// console.log(income);
+
+//======================================================
+
+//bracket Notation
+//third bracket diya access kora
+// console.log(person['age']);
+const boyos = person['age'];
+console.log(boyos);
+
+
+
+console.log(person['fav places']);
