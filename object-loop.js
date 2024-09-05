7b7824fc8bdc0fe82828105e4209ef96add4439c
imrancoder10 <imranhossain1148@gmail.com>
2024-09-05 14:07:59 +0600

    object-loop

---
+++ object-loop.js
@@ -0,0 +1,23 @@
+const mobile = {
+    brand: 'samsung',
+    price: 25000,
+    color: 'black',
+    camera: '12mp',
+    isNew: true
+}
+// ====================================================
+for(const prop in mobile){
+//     console.log(prop);
+//     console.log(mobile[prop]);
+}
+
+//=======================================================
+
+const keys = Object.keys(mobile);
+console.log(keys);
+
+for(const key of keys){
+    console.log(key, ':', mobile[key]);
+}
+
+
